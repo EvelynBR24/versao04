a9a6f450cf6b655c69f1ea095dc103793ace52b5
Evelyn <evelynnbr24@gmail.com>
2024-09-26 20:53:33 -0300

    add media queries

--- javascript/animation.js
+++ javascript/animation.js
@@ -2,7 +2,6 @@
 const sobreImg = document.querySelector('.sobre-img');
 const sobreTexto = document.querySelector('.sobre-texto');
 const banner = document.querySelector('.banner');
-const servico = document.querySelector('.servico li')
 
 const observer = new IntersectionObserver((entries, observer) => {
   entries.forEach(entry => {
@@ -16,4 +15,23 @@ const observer = new IntersectionObserver((entries, observer) => {
 observer.observe(sobreImg);
 observer.observe(sobreTexto);
 observer.observe(banner);
-observer.observe(servico)
+
+// serviços animação
+
+const servicos = document.querySelectorAll('.servico li');
+
+const obse = new IntersectionObserver((entries, observer) => {
+  entries.forEach((entry, index) => {
+    if (entry.isIntersecting) {
+      setTimeout(() => {
+        entry.target.classList.add('animate');
+        obse.unobserve(entry.target); // Parar de observar após a animação
+      }, index * 300); // Adiciona um delay incremental de 300ms para cada item
+    }
+  });
+}, { threshold: 0.5 }); // Animação somente quando 100% visível
+
+// Observa cada serviço
+servicos.forEach(servico => {
+  obse.observe(servico);
+});
